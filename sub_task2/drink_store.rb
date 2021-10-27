444fb317e4d92eb7bc26125a6df5c79e30615d81
Anton <georgesavr6@gmail.com>
2021-10-27 12:05:03 +0300

    total_stock

--- sub_task2/drink_store.rb
+++ sub_task2/drink_store.rb
@@ -5,15 +5,39 @@ class ShopInventory
     end
     
     def item_in_stock
-        drinksNamesInStock = @@inventory.map{|elem| elem[:quantity_by_size].empty?() ? "" : elem[:name]}
-        drinksNamesInStock.delete("")
-        drinksNamesInStock.sort
+        drinks_names_in_stock = @@inventory.map{|elem| elem[:quantity_by_size].empty?() ? "" : elem[:name]}
+        drinks_names_in_stock.delete("")
+        drinks_names_in_stock.sort
     end
 
     def affordable
-        drinksAffordable = @@inventory.map{|elem| elem[:price] < 50 ? elem : ""}
-        drinksAffordable.delete("")
-        drinksAffordable
+        drinks_affordable = @@inventory.map{|elem| elem[:price] < 50 ? elem : ""}
+        drinks_affordable.delete("")
+        drinks_affordable
+    end
+
+    def out_of_stock
+        drinks_out_of_stock = @@inventory.map{|elem| elem[:quantity_by_size].empty?() ? elem : ""}
+        drinks_out_of_stock.delete("")
+        drinks_out_of_stock
+    end
+
+    def how_much_left(name)
+        drinks_how_much_left = @@inventory.map{|elem| elem[:name] == name ? elem : ""}
+        drinks_how_much_left.delete("")
+        drinks_how_much_left[0][:quantity_by_size]
+    end
+
+    def total_stock(name)
+        drinks_total_stock = @@inventory.map{|elem| elem[:quantity_by_size].empty?() ? "" : elem[:quantity_by_size]}
+        drinks_total_stock.delete("")
+        (drinks_total_stock.map{|elem|
+            sum = 0
+            elem.each do |key, value|
+                sum += value
+            end
+            sum
+        }).sum
     end
 end
 
@@ -25,5 +49,13 @@ inventory = [
     ]
 
 obj = ShopInventory.new(inventory)
-puts obj.item_in_stock
-puts obj.affordable
+puts "item_in_stock:"
+puts "#{obj.item_in_stock}\n\n"
+puts "affordable:"
+puts "#{obj.affordable}\n\n"
+puts "out_of_stock:"
+puts "#{obj.out_of_stock}\n\n"
+puts "how_much_left:"
+puts "#{obj.how_much_left("Cola")}\n\n"
+puts "total_stock:"
+puts "#{obj.total_stock("Cola")}\n\n"
