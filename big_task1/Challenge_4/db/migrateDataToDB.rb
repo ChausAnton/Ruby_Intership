d0cf42e1dae70764a721dfc3416030ba47e77f51
Anton <georgesavr6@gmail.com>
2021-11-23 15:20:49 +0200

    challenge 4

---
+++ big_task1/Challenge_4/db/migrateDataToDB.rb
@@ -0,0 +1,17 @@
+require_relative 'dropDB'
+require_relative 'createDB'
+require_relative '../parsFile/pars'
+require 'csv'
+
+def migrateDataToDb()
+    conn = PG.connect(:dbname => 'bigtask', :password => '', :port => 5432, :user => 'chausanton')
+    csvFile = CSV.read("./Data_for_task_1.csv", headers: true)
+    dropDB(conn)
+    createDB(conn)
+    getOfficesInfo(csvFile, conn)
+    getZonesInfo(csvFile, conn)
+    getRoomsInfo(csvFile, conn)
+    getFixturesInfo(csvFile, conn)
+    getMarketingMaterialsInfo(csvFile, conn);
+    conn
+end
